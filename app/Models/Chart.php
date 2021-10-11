9d8b6d19ea4e23f364750294af9b099983befd9b
Ismael Contreras <smartdom.biz@gmail.com>
2021-10-10 23:37:31 -0400

    Avanzada facturación e ingredientes

--- app/Models/Chart.php
+++ app/Models/Chart.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Http\Traits\ChartTrait;
+use App\Traits\ChartTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
--- app/Traits/ChartTrait.php
+++ app/Traits/ChartTrait.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Http\Traits;
+namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
 
