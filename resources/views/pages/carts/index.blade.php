92a9a026908fde7f2c74817ce9daa5be49ed53d1
Ismael Contreras <smartdom.biz@gmail.com>
2021-10-11 10:30:25 -0400

    TODO: CRUD

--- resources/views/pages/carts/index.blade.php
+++ resources/views/pages/carts/index.blade.php
@@ -1,14 +1,14 @@
 @extends('dashboard')
 
 @section('body')
-    <div class="max-w-3xl mx-auto bg-white p-4 rounded-xl">
-        <div class="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 dark:bg-coolGray-900 dark:text-coolGray-100">
+    <div class="max-w-3xl mx-auto bg-white p-2 px-4 rounded-xl">
+        <div class="flex flex-col max-w-3xl p-6 space-y-4 md:px-6 md:py-4  dark:bg-coolGray-900 dark:text-coolGray-100">
             <h2 class="text-xl font-semibold">Artículos para facturar</h2>
             <ul class="flex flex-col divide-y divide-coolGray-700">
                 {{-- Producto --}}
                 @if ($carts->count())
                     @foreach ($carts as $cart)
-                        <li class="flex flex-col py-6 sm:flex-row sm:justify-between">
+                        <li class="flex flex-col pb-3 pt-2 sm:flex-row sm:justify-between">
                             <div class="flex w-full space-x-2 sm:space-x-4">
                                 <img class="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-coolGray-500"
                                     src="{{ $cart->product->Pict }}" alt="Polaroid camera">
@@ -28,10 +28,9 @@
                                         </div>
                                     </div>
                                     <div class="flex text-sm divide-x">
-                                        <div
-                                            class="flex justify-between items-center w-full py-1 pl-0 space-x-1">
-                                            <form class="w-full hover:text-red-400" action="{{ route('charts.destroy', $cart) }}"
-                                                method="POST">
+                                        <div class="flex justify-between items-center w-full py-1 pl-0 space-x-1">
+                                            <form class="w-full hover:text-red-400"
+                                                action="{{ route('charts.destroy', $cart) }}" method="POST">
                                                 @method('delete')
                                                 @csrf
                                                 <button onclick="return confirm('¿Eliminar del carrito?')">
@@ -39,96 +38,101 @@
                                                     <span>Remover</span>
                                                 </button>
                                             </form>
-                                            <form action="{{route('charts.update', $cart)}}" method="POST">
+                                            <form action="{{ route('charts.update', $cart) }}" method="POST">
                                                 @method('put')
                                                 @csrf
                                                 <div class="">
                                                     <div>
                                                         <x-label>Cant.</x-label>
-                                                        <div class="flex space-x-2 items-center">
-                                                            <x-input name="cant" type="number" value="{{ $cart->cant }}"
-                                                                class="w-12 h-8"></x-input>
-                                                                <button class="w-6 h-6 flex items-center justify-center rounded-full bg-green-400 text-white"><span class="fas fa-check"></span></button>
-                                                        </div>
-                                                    </div>
+                                                        <div class="
+                                                    flex space-x-2 items-center">
+                                                    <x-input name="cant" type="number" value="{{ $cart->cant }}"
+                                                        class="w-12 h-8"></x-input>
+                                                    <button
+                                                        class="w-6 h-6 flex items-center justify-center rounded-full bg-green-400 text-white"><span
+                                                            class="fas fa-check"></span></button>
                                                 </div>
-                                            </form>
                                         </div>
-
                                     </div>
+                                    </form>
                                 </div>
+
                             </div>
                         </li>
                     @endforeach
                 @endif
-                <!---->
             </ul>
-            <div class="
-                                                        space-y-1 text-right ">
-                <p>Subtotal:
-                    <input type=" text" disabled class="font-semibold money w-16 text-right" id="spSubtotal"
-                        value="${{ number_format($carts->sum('total'), 2) }}" />
-                </p>
-                <p>Desc:
-                    <input type="text" disabled class="font-semibold  w-16 text-right money" id="spDesc"
-                        value="${{ number_format(0, 2) }}" />
-                </p>
-                <p>Total:
-                    <input type="text" disabled class="font-semibold money w-16 text-right" id="spTotal"
-                        value="${{ number_format($carts->sum('total'), 2) }}" />
-                </p>
-            </div>
-            <div>
-                <form action="{{ route('invoices.store') }}" method="POST" id="formInvoice">
-                    @csrf
-                    <x-input id="subtotal" class="text-right hidden" type="hidden" money="true" name="subtotal" readonly
-                        required value="{{ $carts->sum('total') }}">
-                    </x-input>
-                    <x-input id="total" class="text-right opacity-0" type="hidden" money="true" name="total"
-                        value="{{ $carts->sum('total') }}" readonly required>
-                    </x-input>
-                    <x-input class="text-right hidden" type="hidden" money="true" name="payed" id="payed" required
-                        value="{{ $carts->sum('total') }}" min="0" max="{{ $carts->sum('total') }}">
-                    </x-input>
-                    <div class="lg:flex items-center lg:space-x-2 my-3">
-                        <div class="flex space-x-4 items-center w-full">
-                            <div class="w-full">
-                                <x-label>Cliente</x-label>
-                                <x-select name="client_id" required>
-                                    @foreach ($clients as $client)
-                                        <option value="{{ $client->id }}">{{ $client->name }}
-                                        </option>
-                                    @endforeach
-                                </x-select>
-                            </div>
-                            <div class="w-full">
-                                <x-label>Vendedor</x-label>
-                                <x-input readonly required value="{{ Auth::user()->fullname }}">
-                                </x-input>
-                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                            </div>
+        </div>
+        {{-- Totales --}}
+        <div class="space-y-1 text-right ">
+            <p>Subtotal:
+                <input type=" text" disabled class="font-semibold money w-16 text-right" id="spSubtotal"
+                    value="${{ number_format($carts->sum('total'), 2) }}" />
+            </p>
+            <p>Desc:
+                <input type="text" disabled class="font-semibold  w-16 text-right money" id="spDesc"
+                    value="${{ number_format(0, 2) }}" />
+            </p>
+            <p>Total:
+                <input type="text" disabled class="font-semibold money w-16 text-right" id="spTotal"
+                    value="${{ number_format($carts->sum('total'), 2) }}" />
+            </p>
+        </div>
+        {{--  --}}
+        {{-- Formulario --}}
+        <div>
+            <form action="{{ route('invoices.store') }}" method="POST" id="formInvoice">
+                @csrf
+                <x-input id="subtotal" class="text-right hidden" type="hidden" money="true" name="subtotal" readonly
+                    required value="{{ $carts->sum('total') }}">
+                </x-input>
+                <x-input id="total" class="text-right opacity-0" type="hidden" money="true" name="total"
+                    value="{{ $carts->sum('total') }}" readonly required>
+                </x-input>
+                <x-input class="text-right hidden" type="hidden" money="true" name="payed" id="payed" required
+                    value="{{ $carts->sum('total') }}" min="0" max="{{ $carts->sum('total') }}">
+                </x-input>
+                <div class="lg:flex items-center lg:space-x-2 my-3">
+                    <div class="flex space-x-4 items-center w-full">
+                        <div class="w-full">
+                            <x-label>Cliente</x-label>
+                            <x-select name="client_id" required>
+                                @foreach ($clients as $client)
+                                    <option value="{{ $client->id }}">{{ $client->name }}
+                                    </option>
+                                @endforeach
+                            </x-select>
                         </div>
-                        <div class="">
-                            <div class=" w-full">
-                            <x-label>Descuento</x-label>
-                            <x-input id="discount" class="text-right " money="true" type="text" name="discount" required
-                                value="0" min="0" max="{{ $carts->sum('total') }}">
+                        <div class="w-full">
+                            <x-label>Vendedor</x-label>
+                            <x-input readonly required value="{{ Auth::user()->fullname }}">
                             </x-input>
+                            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
                         </div>
                     </div>
-            </div>
+                    <div class="">
+                                <div class=" w-full">
+                        <x-label>Descuento</x-label>
+                        <x-input id="discount" class="text-right " money="true" type="text" name="discount" required
+                            value="0" min="0" max="{{ $carts->sum('total') }}">
+                        </x-input>
+                    </div>
+                </div>
             </form>
-            <div class="flex justify-end space-x-4">
-                <a href="{{ route('products.index') }}" class="px-6 py-2 border rounded-md dark:border-indigo-400">Volver
-                </a>
-
-                <button type="submit" form="formInvoice"
-                    class="px-6 py-2 border rounded-md dark:bg-indigo-400 dark:text-coolGray-900 dark:border-indigo-400">
-                    <span class="sr-only sm:not-sr-only">Continue to</span>Checkout
-                </button>
-            </div>
         </div>
-    </div>
+        {{--  --}}
+        {{-- Botones --}}
+        <div class="flex justify-end space-x-4">
+            <a href="{{ route('products.index') }}" class="px-6 py-2 border rounded-md dark:border-indigo-400">Volver
+            </a>
 
+            <button type="submit" form="formInvoice"
+                class="px-6 py-2 border rounded-md dark:bg-indigo-400 dark:text-coolGray-900 dark:border-indigo-400">
+                Cobrar
+            </button>
+        </div>
+        {{--  --}}
     </div>
+    <!---->
+
 @endsection
