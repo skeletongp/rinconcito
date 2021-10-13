2fd4ebbb07acc04d426e232b295dfcd737736e79
Ismael Contreras <smartdom.biz@gmail.com>
2021-10-13 02:00:29 -0400

    TODO: CRUD, Hecho CRUD productos y usuarios

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -3,19 +3,19 @@
 @section('body')
     <div class="max-w-4xl mx-auto bg-white p-4 rounded-xl">
         <h1 class="text-center font-bold uppercase text-xl my-4">Usuarios registrados</h1>
-        <ul class="cards grid grid-cols-1 md:grid-cols-2  m-8">
+        <ul class="cards grid grid-cols-1 md:grid-cols-2 m-3 lg:m-8">
             @if ($users->count())
                 @foreach ($users as $user)
                     <li class=" shadow-xl max-w-sm bg-white p-2 rounded-xl ">
                         <div class="w-full h-24 bg-cover bg-center flex flex-col justify-center rounded-xl "
                             style="background-image: url(/fondo.jpg)">
-                            <div class="  h-full flex items-center space-x-3 bg-gray-50 p-4 bg-opacity-60 relative">
+                            <div class="  h-full flex items-center space-x-3 bg-gray-50 p-2 md:p-4 bg-opacity-60 relative">
                                 <div class="w-16 h-16 rounded-full bg-cover bg-center "
                                     style="background-image: url('{{ $user->photo }}')">
                                 </div>
                                 <div class="flex flex-col justify-end">
                                     <span class="text-xl font-bold">{{ $user->fullname }}</span>
-                                    <p class="text-sm w-max font-bold leading-3 mt-2">
+                                    <p class="text-sm w-max font-bold leading-4 mt-2">
                                         {{ $user->phone }}
                                         <br>{{ $user->role }}
                                         <br>{{ $user->email }}
