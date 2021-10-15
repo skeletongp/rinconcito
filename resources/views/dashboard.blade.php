2c03a70ed495a281f320e808b9904b67411a62a6
Ismael Contreras <smartdom.biz@gmail.com>
2021-10-15 01:51:22 -0400

    Hecha la parte de las ventas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,14 +12,14 @@
                 <div class="flex justify-between">
                     <div class="p-1 mx-3 inline-flex items-center">
                         <i class="fas fa-bars pr-2 text-black lg:text-2xl cursor-pointer " onclick="sidebarToggle()"></i>
-                        <a href="{{route('home')}}" class="text-black p-2">{{env('COMPANY_NAME','CoffeTech')}}</a>
+                        <a href="{{route('home')}}" class="text-black p-2 font-bold uppercase text-xl md:text-2xl font-sans">{{env('COMPANY_NAME','CoffeTech')}}</a>
                     </div>
                     <x-dropdown>
                         <x-slot name="trigger">
                             <div class="flex items-center space-x-1 cursor-pointer mt-3">
                                 <img class="inline-block h-8 w-8 rounded-full" src="{{ Auth::user()->photo }}" alt="">
                                 <a href="#"
-                                    class="text-white p-2 no-underline hidden md:block lg:block">{{ Auth::user()->fullname }}</a>
+                                    class="text-white p-2 no-underline hidden font-bold md:block lg:block">{{ Auth::user()->fullname }}</a>
                             </div>
                         </x-slot>
                         <x-slot name="content">
@@ -63,7 +63,7 @@
             <footer class="fixed bottom-0 z-30 bg-left text-blue-900 font-bold p-2 flex justify-between w-screen lg:px-8"
                 style="background-image: url('/fondo.jpg')">
                 <div class="">&copy; 2021</div>
-            <div class=" f">Creado by: <a
+            <div class=" f">Creado por: <a
                         href="https://ismaeldigitador.com/" target=" _blank">Ismael Contreras</a></div>
             </footer>
             <!--/footer-->
